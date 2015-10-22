589da9b520272807565b4cdbfd7c3b0616c2399c
Yu Wu <wuyucsu@gmail.com>
2015-10-21 23:09:12 -0500

    Add web resources: js, css, img

---
+++ WebContent/META-INF/scripts/showPic.js
@@ -0,0 +1,43 @@
+
+
+function showPic(pic) {
+	if (!document.getElementById("placeholder")) return false;
+	var source = pic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	if (placeholder.nodeName != "IMG") return false;
+	placeholder.setAttribute("src", source);
+	if (document.getElementById("description")) {
+		var description = document.getElementById("description");
+		if (description.firstChild.nodeType == 3) {
+			var text = pic.getAttribute("Title") ? pic.getAttribute("Title") : "";
+			description.firstChild.nodeValue = text;
+		}
+	}
+	return true;
+}
+
+function countBodyChildren() {
+	var body_element = document.getElementsByTagName('body')[0];
+	//alert(body_element.childNodes.length);
+	alert(body_element.nodeType);
+}
+
+
+function prepareGallery() {
+	if (!document.getElementById || !document.getElementsByTagName) {
+		return false;
+	}
+	var gallery = document.getElementById("image_list");
+	if (!gallery) {
+		return false;
+	}
+	var links = gallery.getElementsByTagName("a");
+	for (var i = 0; i < links.length; i++) {
+		links[i].onclick = function() {
+			return showPic(this) ? false : true;	
+		}
+	}
+}
+
+//window.onload = countBodyChildren;
+window.onload = prepareGallery;
